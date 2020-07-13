8e3a5260b8790ed0f4a2d731f1eee7a1988d62b1
cdeng <Chong.Deng@infocus.com>
2020-07-12 22:22:12 -0700

    added login response with JWT

--- user-center/src/main/java/com/avistar/usercenter/service/user/UserService.java
+++ user-center/src/main/java/com/avistar/usercenter/service/user/UserService.java
@@ -1,10 +1,13 @@
 package com.avistar.usercenter.service.user;
 
 import com.avistar.usercenter.dao.user.UserMapper;
+import com.avistar.usercenter.domain.dto.user.UserLoginDTO;
 import com.avistar.usercenter.domain.entity.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Slf4j
 @Service
@@ -15,6 +18,32 @@ public class UserService {
     public User findById(Integer id) {
         // select * from user where id = #{id}
         return userMapper.selectByPrimaryKey(id);
+    }
+
+    public User login(UserLoginDTO loginDTO){
+
+        User user = this.userMapper.selectOne(
+                User.builder()
+                        .wxNickname(loginDTO.getWxNickname())
+                        .build()
+        );
+
+        if (user == null) {
+            User userToSave = User.builder()
+                    .wxId("random")
+                    .bonus(300)
+                    .wxNickname(loginDTO.getWxNickname())
+                    .avatarUrl(loginDTO.getAvatarUrl())
+                    .roles("user")
+                    .createTime(new Date())
+                    .updateTime(new Date())
+                    .build();
+            this.userMapper.insertSelective(
+                    userToSave
+            );
+            return userToSave;
+        }
+        return user;
     }
 
 
