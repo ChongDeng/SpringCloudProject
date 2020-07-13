7c3eb07f598a7bf067eb6003326a4a508254b23d
cdeng <Chong.Deng@infocus.com>
2020-07-12 17:47:38 -0700

    added Feign logging

--- content-center/src/main/java/com/avistar/contentcenter/feignclient/UserCenterFeignClient.java
+++ content-center/src/main/java/com/avistar/contentcenter/feignclient/UserCenterFeignClient.java
@@ -1,12 +1,13 @@
 package com.avistar.contentcenter.feignclient;
 
+import com.avistar.contentcenter.configuration.UserCenterlFeignConfiguration;
 import com.avistar.usercenter.domain.dto.user.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "user-center", configuration = GlobalFeignConfiguration.class)
-@FeignClient(name = "user-center")
+@FeignClient(name = "user-center", configuration = UserCenterlFeignConfiguration.class)
 public interface UserCenterFeignClient {
     /**
      * http://user-center/users/{id}
